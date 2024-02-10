3c643241b750e809d27523c429cec95a2eda609d
Tufi007 <taufeeqr346@gmail.com>
2024-02-10 21:45:17 +0530

    added dotenv file

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
  const express = require('express');
  const app= express();
- const fs= require('fs');
  const router= require('./route/urlroute')
 app.use(express.json());
-const PORT = 8000;
 app.use('/',router);
-app.listen(PORT,(req,res)=>{
-   
-    console.log('server connected');
-})
-
 module.exports= app;
