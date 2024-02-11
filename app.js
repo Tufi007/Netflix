ca751b6509df2c93fd788ca99334425dd815556a
Tufi007 <taufeeqr346@gmail.com>
2024-02-11 12:13:26 +0530

    still orginizing

--- app.js
+++
@@ -1,7 +0,0 @@
- const express = require('express');
- const app= express();
- const router= require('./route/urlroute')
-app.use(express.json());
-app.use(express.static('./static'));
-app.use('/',router);
-module.exports= app;
--- route/urlroute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const moviecrud= require('./../controller/moviecrud');
-const router = express.Router();
-router.route("/").get(moviecrud.getallmovies).post(moviecrud.creatmovie);
-
-router.route("/:id").get(moviecrud.getmovie).patch(moviecrud.updatemovie).delete(moviecrud.deletemovie);
-
-module.exports = router;
