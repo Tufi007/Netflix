98f4d72801b5cab3e112e4991e68f83bab83df1c
Tufi007 <taufeeqr346@gmail.com>
2024-02-13 00:20:52 +0530

    added simple mongo db crud api

--- expressjs-mongoose-mongodb/app.js
+++ expressjs-mongoose-mongodb/app.js
@@ -1,10 +1,11 @@
- const express = require('express');
- const app= express();
- const router= require('./route/urlroute');
-const morgan = require('morgan');
+const express = require("express");
+const app = express();
+const router = require("./route/urlroute");
+const morgan = require("morgan");
 app.use(express.json());
-app.use(express.static('./static'));
-if(process.env.NODE_ENV=== 'development') app.use(morgan("dev"));
 
-app.use('/',router);
-module.exports= app;
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use("/", router);
+app.use(express.static("./static"));
+module.exports = app;
--- expressjs-mongoose-mongodb/controller/moviecrud.js
+++ expressjs-mongoose-mongodb/controller/moviecrud.js
@@ -1,75 +1,67 @@
 const express = require("express");
 const fs = require("fs");
-let movies = JSON.parse(fs.readFileSync("./data.json"));
+const movies = require("./../model/moviemodel");
+// let movies = JSON.parse(fs.readFileSync("./data.json"));
 const responsefunction = (status, data) => {
-    return {
-      status: status,
-      data: data,
-    };
+  return {
+    status: status,
+    length: data.length,
+    data: data,
   };
-  exports.getallmovies = (req, res) => {
-    res.status(200).json({
-      status: "succes",
-      data: movies,
-    });
-  };
-  exports.creatmovie = (req, res) => {
-    
-    if (!req.body.id || !req.body.movie)
-      return res.json(responsefunction("failed", req.body));
-    if (req.body.id || req.body.movie) {
-      
-      movies.push(req.body);
-      let newmovies = JSON.stringify(movies);
-      
-      fs.writeFile("./data.json",newmovies, () => {
-        
-        return res.status(201).json(responsefunction("succes", req.body));
-      });
+};
+exports.getallmovies = async (req, res) => {
+  data = await movies.find({});
+  res.status(200).json(responsefunction("succes", data));
+};
+exports.creatmovie = async (req, res) => {
+  try {
+    const data = req.body;
+    const movietitle = req.body.title;
+    console.log(movietitle);
+    const found = await movies.find({ title: movietitle });
+    console.log(found);
+    if (found) {
+      const movie = await movies.create(data);
+      res.status(201).json(responsefunction("succes", movie));
     }
-  };
-  exports.getmovie = (req, res) => {
-    console.log(req.params);
-    const foundmovie = movies.find((movie) => {
-      return movie.id == req.params.id * 1;
-    });
-    if (!foundmovie)
-      return res
-        .status(404)
-        .json(responsefunction("failed no id", req.params.id));
-    if (foundmovie)
-      return res.status(200).json(responsefunction("succes", foundmovie));
-  };
-  exports.updatemovie = (req,res) => {
-    const patch=req.body;
-    const id = req.params.id*1;
-    const foundmovie= movies.find(movie => movie.id=== id);
-    if (!foundmovie) return res.status(404).json(responsefunction('cannot update as id not',id));
-    if (foundmovie){
-      let index= movies.indexOf(foundmovie);
-      Object.assign(foundmovie,req.body);
-      movies[index]=foundmovie;
-      console.log(foundmovie);
-      const newmovie= JSON.stringify(movies);
-      fs.writeFile('./data.json',newmovie,(err)=>{
-        res.status(202).json(responsefunction('updated',foundmovie));
-      });
-      
-    }
-  };
-  exports.deletemovie = (req,res) => {
-    const id = req.params.id*1;
-    const foundmovie= movies.find((movie) => movie.id === id);
-    console.log(foundmovie);
-    if(!foundmovie) return res.status(404).json(responsefunction('no data found'));
-    if(foundmovie){
-      const index= movies.indexOf(foundmovie);
-      console.log(index);
-      movies.splice(index,1);
-      const newmovie= JSON.stringify(movies);
-      fs.writeFile('./data.json',newmovie,(err)=>{
-       res.status(202).json(responsefunction('succesfully deleted'));
-      });
-      
-    }
+  } catch (err) {
+    res
+      .status(404)
+      .json(responsefunction(`failed to enter the movies data`, err.message));
   }
+};
+exports.getmovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const found = await movies.find({ title: id });
+    console.log(found);
+    if (found) res.status(200).json(responsefunction("succes", found));
+  } catch (err) {
+    res.status(404).json(responsefunction("failed", err.message));
+  }
+};
+exports.updatemovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const found = await movies.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (found) {
+      res.status(200).json(responsefunction("succes", found));
+    }
+  } catch (err) {
+    res.status(404).json(responsefunction("failed", err.message));
+  }
+};
+
+exports.deletemovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const found = await movies.findByIdAndDelete(id);
+    if (found) res.status(200).json(responsefunction("status", found));
+  } catch (err) {
+    res.status(404).json(responsefunction("failed", err.message));
+  }
+};
